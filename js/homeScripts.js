909c7395736248c3d26e13d3e89194463e66a80d
Chris <chrisanthonysmith91@gmail.com>
2018-08-06 22:55:04 -0700

    splash page design finished. need to implement Scrollify plugin

--- js/homeScripts.js
+++ js/homeScripts.js
@@ -1,13 +1,9 @@
 jQuery(document).ready(function($){
-        
+//-menu toggle on click-//      
     $('#menuToggle').click(function(){
         $('#menu').toggleClass( 'show', 600, 'swing' );
     });
-
-
-
-
-
+//-particle effects-//
     particlesJS("particles-js", {
         "particles":{
             "number":{
@@ -49,20 +45,45 @@ jQuery(document).ready(function($){
         },
         "retina_detect":true}
     );
-    // var count_particles, stats, update; 
-    // stats = new Stats; 
-    // stats.setMode(0); 
-    // stats.domElement.style.position = 'absolute'; 
-    // stats.domElement.style.left = '0px'; 
-    // stats.domElement.style.top = '0px'; 
-    // document.body.appendChild(stats.domElement); 
-    // count_particles = document.querySelector('.js-count-particles'); 
-    // update = function() { 
-    //     stats.begin(); stats.end(); 
-    //     if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) { count_particles.innerText = window.pJSDom[0].pJS.particles.array.length; } 
-    //     requestAnimationFrame(update); 
-    // }; 
-    // requestAnimationFrame(update);;
+
+
+//-smooth scroll on click-//    
+    // Select all links with hashes
+    $('a[href*="#"]')
+        // Remove links that don't actually link to anything
+        .not('[href="#"]')
+        .not('[href="#0"]')
+        .click(function(event) {
+        // On-page links
+        if (
+            location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+            && 
+            location.hostname == this.hostname
+        ) {
+            // Figure out element to scroll to
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            // Does a scroll target exist?
+            if (target.length) {
+            // Only prevent default if animation is actually gonna happen
+            event.preventDefault();
+            $('html, body').animate({
+                scrollTop: target.offset().top
+            }, 1000, function() {
+                // Callback after animation
+                // Must change focus!
+                var $target = $(target);
+                $target.focus();
+                if ($target.is(":focus")) { // Checking if the target was focused
+                return false;
+                } else {
+                $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+                $target.focus(); // Set focus again
+                };
+            });
+            }
+        }
+    });
 
 
 
