1e7a7d68c616ad5c1e190b890e56c0e89c9bd063
Chris <chrisanthonysmith91@gmail.com>
2018-08-10 23:00:09 -0700

    Homepage almost done...need to address compatibility issues with microsoft edge

--- js/homeScripts.js
+++ js/homeScripts.js
@@ -1,92 +1,18 @@
 jQuery(document).ready(function($){
 //-menu toggle on click-//      
     $('#menuToggle').click(function(){
+        $('#menuToggle').toggleClass('rotate');
         $('#menu').toggleClass( 'show', 600, 'swing' );
     });
-//-particle effects-//
-    particlesJS("particles-js", {
-        "particles":{
-            "number":{
-                "value":80,
-                "density":{"enable":true,"value_area":800}},
-            "color":{"value":"#5F94B4"},
-            "shape":{
-                "type":"circle",
-                "stroke":{"width":0,"color":"#000000"},
-                "polygon":{"nb_sides":5},
-                "image":{"src":"","width":100,"height":100}},
-            "opacity":{
-                "value":0.5,
-                "random":false,
-                "anim":{
-                    "enable":false,
-                    "speed":1,
-                    "opacity_min":0.1,
-                    "sync":false}},
-            "size":{"value":3,"random":true,"anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}},
-            "line_linked":{
-                "enable":true,
-                "distance":150,
-                "color":"#ffffff",
-                "opacity":0.3,
-                "width":1},
-            "move":{"enable":true,"speed":6,"direction":"none","random":false,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}
-        },
-        "interactivity":{
-            "detect_on":"canvas","events":{
-                "onhover":{"enable":true,"mode":"grab"},
-                "onclick":{"enable":true,"mode":"repulse"},"resize":true},
-            "modes":{
-                "grab":{"distance":400,"line_linked":{"opacity":1}},
-                "bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},
-                "repulse":{"distance":200,"duration":0.4},
-                "push":{"particles_nb":4},
-                "remove":{"particles_nb":2}}
-        },
-        "retina_detect":true}
-    );
-
-
-//-smooth scroll on click-//    
-    // Select all links with hashes
-    $('a[href*="#"]')
-        // Remove links that don't actually link to anything
-        .not('[href="#"]')
-        .not('[href="#0"]')
-        .click(function(event) {
-        // On-page links
-        if (
-            location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-            && 
-            location.hostname == this.hostname
-        ) {
-            // Figure out element to scroll to
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            // Does a scroll target exist?
-            if (target.length) {
-            // Only prevent default if animation is actually gonna happen
-            event.preventDefault();
-            $('html, body').animate({
-                scrollTop: target.offset().top
-            }, 1000, function() {
-                // Callback after animation
-                // Must change focus!
-                var $target = $(target);
-                $target.focus();
-                if ($target.is(":focus")) { // Checking if the target was focused
-                return false;
-                } else {
-                $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-                $target.focus(); // Set focus again
-                };
+// helper function for animations
+    $.fn.extend({
+        animateCss: function (animationName) {
+            var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+            this.addClass('animated ' + animationName).one(animationEnd, function() {
+                $(this).removeClass('animated ' + animationName);
             });
-            }
         }
     });
-
-
-
 
 
 });
